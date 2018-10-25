8de135020d5c5a9ed89541df2f611b8f3d29b259
Robin Mills <robin@clanmills.com>
2018-10-25 09:38:09 +0000

    Exiv2 v0.27.0.1 RC1 for team review

--- website/bin/download.sh
+++ website/bin/download.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 # Create download tables
 
 basedir=.
@@ -10,11 +10,20 @@ buttons=__download_buttons__
 rm -rf $basedir/var/$table
 rm -rf $basedir/var/$buttons
 
-for P in trunk macosx linux cygwin mingw msvc; do
-	p=exiv2-$version-$P.tar.gz
-	size=$(ls -la html/builds/$p | cut -d' ' -f 5)
-	date=$(stat -c "%y" html/builds/$p | cut -d' ' -f 1)
-	checkSum=$(sha256sum html/builds/$p  | cut -d' ' -f 1)
+##
+# clean up from last time and copy builds html/builds
+if [ -e   $basedir/html/builds ]; then rm -rf $basedir/html/builds ; fi
+mkdir -p  $basedir/html/builds
+cp        $basedir/builds/*                   $basedir/html/builds
+
+for P in Darwin Linux CYGWIN MinGW-64 MinGW-32 MSVC Source ; do
+    ext=tar.gz
+	if [ $P == MSVC ]; then ext=zip ; fi
+	p=exiv2-$version-$P.$ext
+
+	size=$(ls -la        html/builds/$p | cut -d' ' -f 5)
+	date=$(stat -c "%y"  html/builds/$p | cut -d' ' -f 1)
+	checkSum=$(sha256sum html/builds/$p | cut -d' ' -f 1)
 
 	echo "<tr>  \
             <td>Exiv2 v$version $P</td> \
@@ -26,12 +35,10 @@ for P in trunk macosx linux cygwin mingw msvc; do
 
 	platform=$P
 	config="64 bit shared libraries"
-	if [ "$platform" == macosx ]; then platform="MacOSX"       ; fi
-	if [ "$platform" == cygwin ]; then platform="Cygwin"       ; fi
-	if [ "$platform" == mingw  ]; then platform="MinGW"        ; config="32 bit shared libraries"                     ; fi
-	if [ "$platform" == msvc   ]; then platform="Visual Studio"; config="32 & 64 bit DLLs for MSVC 2005/8/10/12/13/15"; fi
-	if [ "$platform" == linux  ]; then platform="Linux"        ; fi
-	if [ "$platform" != trunk  ]; then
+	if [ "$platform" == Darwin   ]; then platform="MacOSX"                 ; fi
+	if [ "$platform" == MinGW-32 ]; then config="32 bit shared libraries"  ; fi
+	if [ "$platform" == MSVC     ]; then platform="Visual Studio"; config="64 bit DLLs for<br>Visual Studio 2017"; fi
+	if [ "$platform" != Source   ]; then
   	  echo "<tr><td>$platform<h3></td><td>$config</td> \
 	        <td> \
 	          <p3 class=\"text-center\"> \
