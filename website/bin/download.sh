9c4804ac391c14cebbd7283a9b30004157b7a081
Robin Mills <robin@clanmills.com>
2018-10-31 15:08:56 +0000

    Documentation update.  Modified bin/download.sh to publish whatever builds are in the website/builds directory

--- website/bin/download.sh
+++ website/bin/download.sh
@@ -15,25 +15,36 @@ rm -rf $basedir/var/$buttons
 if [ -e   $basedir/html/builds ]; then rm -rf $basedir/html/builds ; fi
 mkdir -p  $basedir/html/builds
 
-for P in Darwin Linux CYGWIN MinGW64 MinGW32 MSVC Source ; do
-    ext=tar.gz
-	if [ $P == MSVC ]; then ext=zip ; fi
-	p=exiv2-$version-$P.$ext
-    cp    -p  $basedir/builds/*${P}*  $basedir/html/builds/$p
+for P in $basedir/builds/*
+do
+    # P = ./builds/exiv2-0.27.0.2-CYGWIN-2018:10:31_11:57:09.tar.gz
+    platform=$(echo $P | cut -d- -f 3   ) # CYGWIN
+    S=$(       echo $P | cut -d- -f 1-3 ) # ./builds/exiv2-0.27.0.2-CYGWIN
+    stub=$(    echo $S | cut -d/ -f 3-  ) # exiv2-0.27.0.2-CYGWIN
+    ext=$(     echo $P | cut -d. -f 6-  ) # tar.gz
+    p=$stub.$ext                          # exiv2-0.27.0.2-CYGWIN.tar.gz
+    if [ 1 -eq 2 ]; then
+		echo P = $P
+		echo platform = $platform
+		echo S = $S
+		echo stub = $stub
+		echo ext  = $ext
+		echo p = $p
+    fi
 
+    cp    -p  $P  $basedir/html/builds/$p
 	size=$(ls -la        html/builds/$p | cut -d' ' -f 5)
 	date=$(stat -c "%y"  html/builds/$p | cut -d' ' -f 1-2 | cut -d: -f 1-2)
 	checkSum=$(sha256sum html/builds/$p | cut -d' ' -f 1)
 
 	echo "<tr>  \
-            <td>Exiv2 v$version $P</td> \
-            <td><a href=\"builds/${p}\">${p}</a></td> \
+            <td>Exiv2 v$version $platform</td> \
+            <td><a href=\"builds/$p\">$p</a></td> \
             <td>$size</td> \
             <td class=\"text-nowrap\">$date</td> \
             <td>$checkSum</td> \
           </tr>"  >> $basedir/var/$table
 
-	platform=$P
 	config="64 bit shared libraries"
 	if [ "$platform" == Darwin   ]; then platform="MacOSX"                 ; fi
 	if [ "$platform" == MinGW32  ]; then config="32 bit shared libraries"  ; fi
