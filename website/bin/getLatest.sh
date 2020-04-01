46a8c606fb07025f00746448c67cb110599bc077
Robin Mills <robin@clanmills.com>
2020-04-01 18:48:31 +0000

    Exiv2 v0.27.3.0 maitenance.

--- website/bin/getLatest.sh
+++ website/bin/getLatest.sh
@@ -3,17 +3,25 @@
 builds=~/Jenkins/builds/Latest
 out=builds
 
+if [ ! -d $out ]; then
+    echo "*** create a directory called $out ***"
+    exit
+fi
+if [ ! -d ~/Jenkins/builds/Latest ]; then
+    echo "*** ~/Jenkins/builds/Latest does not exist ***"
+    exit
+fi
+
 ##
 # clean up anything from last time
-cd  $out
-rm -rf *
+rm -rf $out/*
 
 ##
 # copy the builds and chmod them
-cp -p "$builds"/* .
+cp -p "$builds"/* $out
 echo ++++++++++++++++++
-chmod 0444 *
-ls -lh     .
+chmod 0444 $out/*
+ls -lh     $out/*
 
 # That's all folks!
 ##
