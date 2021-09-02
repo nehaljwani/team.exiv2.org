52477f064637137f8d365945718ed6a26e182baf
Robin Mills <robin@clanmills.com>
2021-09-02 02:55:27 +0000

    fix permissions to index.html on clanmills.com

--- book/export.sh
+++ book/export.sh
@@ -5,6 +5,7 @@ for i in $(find . -type f -depth 1); do cp -v $i ~/clanmills/exiv2/book ; done
 cd ~/clanmills
 mv exiv2/book/IMaEA.html exiv2/book/index.html
 ./syncup exiv2
+ssh clanmil1@clanmills.com 'chmod 644 public_html/exiv2/book/index.html'
 
 # That's all Folks!
 ##
