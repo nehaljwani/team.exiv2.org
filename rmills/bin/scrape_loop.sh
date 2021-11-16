6fc021116a34cc8a8ff5b15adebcdc6abb1524a2
Robin Mills <robin@clanmills.com>
2021-11-16 20:06:46 +0000

    team/rmills/bin scrape scripts development

--- rmills/bin/scrape_loop.sh
+++ rmills/bin/scrape_loop.sh
@@ -3,8 +3,8 @@
 result=0
 d=default.shtml
 count=$(find . -name $d | wc -l)
-if [ "$count" != "1" ]; then
-    echo "***" loop in $PWD "***"
+if [ "$count" -ne "1" ]; then
+    echo "***" loop in $PWD count = "$count" "***"
     result=1
     echo $PWD >> /tmp/scrape_loops
 fi
